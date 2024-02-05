7f47b5497b2fa7d992e11ef8564b61425a662978
SarikAnwar01 <sarikanwar007@gmail.com>
2024-02-05 17:06:53 +0545

    cache logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    let registration;
+
+    if ("serviceWorker" in navigator) {
+      const onLoad = () => {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then((reg) => {
+            registration = reg;
+            console.log("Service Worker registered:", registration);
+          })
+          .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+          });
+      };
+
+      window.addEventListener("load", onLoad);
+
+      return () => {
+        // Cleanup function: Remove event listener and unregister service worker
+        window.removeEventListener("load", onLoad);
+        if (registration) {
+          registration.unregister().then(() => {
+            console.log("Service Worker unregistered:", registration);
+          });
+        }
+      };
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
