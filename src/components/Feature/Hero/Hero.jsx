efb8584ff6b6fe20662c45c497ed0c3bbd3468dd
abdulchaman <abdulchaman123@gmail.com>
2024-02-21 23:07:34 +0530

    hero section feature page added

--- src/components/Feature/Hero/Hero.jsx
+++ src/components/Feature/Hero/Hero.jsx
@@ -4,7 +4,21 @@ import "./hero.css";
 const Hero = () => {
     return (
         <div className='ft_Hero-wrap'>
-            
+            <div className='ft_Hero-sec'>
+                <div className='ft_Hr-ttl'>
+                    <h1>
+                        A digital<br></br>
+                        platform<br></br>
+                        crafted to<br></br>
+                        empower our<br></br>
+                        educational<br></br>
+                        organizations
+                    </h1>
+                </div>
+                <div className='ft_Hr-img'>
+                    <img src="assets/img/hr-img.png" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
