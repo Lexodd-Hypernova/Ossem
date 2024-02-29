fd8c88e8b66f85893f35e0be67441dccf1ea73ec
abdulchaman <abdulchaman123@gmail.com>
2024-03-01 00:10:24 +0530

    about page sections done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Hero from '../components/About/Hero';
 import Technology from '../components/About/Technology';
 import Cliente from "../components/Home/Cliente/Cliente";
-import AboutVideo from '../components/About/AboutVideo';
-// import Founder from '../components/About/Founder';
+import Founder from '../components/About/Founder';
+import AboutVideoFile from '../components/About/AboutVideoFile';
 
 const About = () => {
     return (
         <>
             <Hero></Hero>
             <Technology></Technology>
-            <AboutVideo></AboutVideo>
-            {/* <Founder></Founder> */}
+            <AboutVideoFile></AboutVideoFile>
+            <Founder></Founder>
             <Cliente></Cliente>
         </>
     )
