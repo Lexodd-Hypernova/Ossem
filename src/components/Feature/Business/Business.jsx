723675e48f7e8790cabe7534f7dd10a16b9ced6a
abdulchaman <abdulchaman123@gmail.com>
2024-03-02 00:32:20 +0530

    App features section of feature page fixed

---
+++ src/components/Feature/Business/Business.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./business.css";
+
+const Business = () => {
+    return (
+        <div className='section__About'>Business</div>
+    )
+}
+
+export default Business
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from 'react';
 import Overview from '../components/Feature/Overview/Overview';
 import Hero from "../components/Feature/Hero/Hero";
 import AppFeatures from '../components/Feature/AppFeatures/AppFeatures';
+import Business from '../components/Feature/Business/Business';
 
 import ScrollToTop from "../components/ScrollToTop";
 import Lenis from '@studio-freight/lenis';
+
+// import ScrollableSection from '../components/Feature/AppFeatures/ScrollableSection';
 
 const Features = () => {
 
@@ -28,6 +31,8 @@ const Features = () => {
             <Hero></Hero>
             <Overview></Overview>
             <AppFeatures></AppFeatures>
+            {/* <ScrollableSection></ScrollableSection> */}
+            <Business></Business>
             <ScrollToTop></ScrollToTop>
         </>
     )
