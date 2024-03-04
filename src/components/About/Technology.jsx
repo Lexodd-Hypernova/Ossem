7fa64bd58ab736fd49a0bd914f7b63c1c9ac1cdd
abdulchaman <abdulchaman123@gmail.com>
2024-03-05 01:00:48 +0530

    about page mobile responsived

--- src/components/About/Technology.jsx
+++ src/components/About/Technology.jsx
@@ -17,6 +17,9 @@ const Technology = () => {
                 <div className="abt_carousel">
                     <HorizontalScrollCarousel />
                 </div>
+                <div className="abt__Mob">
+                    <MobScroll></MobScroll>
+                </div>
             </div>
         </div>
     );
@@ -38,110 +41,202 @@ const HorizontalScrollCarousel = () => {
     const y = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
 
     return (
-        <section ref={targetRef} className="scroll__Cnt">
-            <div className="scl__Wrap">
-                <motion.div style={{ y }} className="crd__Item">
-                    {/* {cards.map((card) => {
+        <>
+
+            <section ref={targetRef} className="scroll__Cnt">
+                <div className="scl__Wrap">
+                    <motion.div style={{ y }} className="crd__Item">
+                        {/* {cards.map((card) => {
                         return <Card card={card} key={card.id} />;
                     })} */}
 
-                    <div className="card_abt">
-                        <div className="card__In">
-                            <Fade direction="up" triggerOnce>
-                                <div className='abt_Row abt_Row1'>
-                                    <div className='abt_gl-box'>
-                                        <div className='abt_gl-cnt'>
-                                            <div className='gl_ttl'>
-                                                <h3>Lorem</h3>
+                        <div className="card_abt">
+                            <div className="card__In">
+                                <Fade direction="up" triggerOnce>
+                                    <div className='abt_Row abt_Row1'>
+                                        <div className='abt_gl-box'>
+                                            <div className='abt_gl-cnt'>
+                                                <div className='gl_ttl'>
+                                                    <h3>Lorem</h3>
+                                                </div>
+                                                <div className='gl_cn'>
+                                                    <div className='mn_ttl'>Ipsum</div>
+                                                    <div className='mn_Num'>01</div>
+                                                </div>
+                                                <div className='gl_img'>
+                                                    <img src="assets/img/gl1.png" alt="" />
+                                                </div>
                                             </div>
-                                            <div className='gl_cn'>
-                                                <div className='mn_ttl'>Ipsum</div>
-                                                <div className='mn_Num'>01</div>
-                                            </div>
-                                            <div className='gl_img'>
-                                                <img src="assets/img/gl1.png" alt="" />
+                                        </div>
+                                        <div className='abt_txt'>
+                                            <h2>
+                                                About ProdServ<br></br><span>Technologies</span>
+                                            </h2>
+                                        </div>
+
+                                    </div>
+                                </Fade>
+
+                            </div>
+                        </div>
+                        <div className="card_abt">
+                            <div className="card__In">
+                                <Fade direction="up" triggerOnce>
+                                    <div className='abt_Row abt_Row2'>
+                                        <div className='abt_txt'>
+                                            <p>
+                                                "Lorem ipsum dolor sit amet,
+                                                consectetur adipiscing elit,
+                                                sed do eiusmod tempor incididunt
+                                                ut labore et dolore magna aliqua."
+                                            </p>
+                                        </div>
+                                        <div className='abt_gl-box'>
+                                            <div className='abt_gl-cnt'>
+                                                <div className='gl_ttl'>
+                                                    <h3>Lorem</h3>
+                                                </div>
+                                                <div className='gl_cn'>
+                                                    <div className='mn_ttl'>Ipsum</div>
+                                                    <div className='mn_Num'>01</div>
+                                                </div>
+                                                <div className='gl_img'>
+                                                    <img src="assets/img/gl2.png" alt="" />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='abt_txt'>
-                                        <h2>
-                                            About ProdServ<br></br><span>Technologies</span>
-                                        </h2>
-                                    </div>
+                                </Fade>
 
-                                </div>
-                            </Fade>
-
+                            </div>
                         </div>
-                    </div>
-                    <div className="card_abt">
-                        <div className="card__In">
-                            <Fade direction="up" triggerOnce>
-                                <div className='abt_Row abt_Row2'>
-                                    <div className='abt_txt'>
-                                        <p>
-                                            "Lorem ipsum dolor sit amet,
-                                            consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt
-                                            ut labore et dolore magna aliqua."
-                                        </p>
-                                    </div>
-                                    <div className='abt_gl-box'>
-                                        <div className='abt_gl-cnt'>
-                                            <div className='gl_ttl'>
-                                                <h3>Lorem</h3>
-                                            </div>
-                                            <div className='gl_cn'>
-                                                <div className='mn_ttl'>Ipsum</div>
-                                                <div className='mn_Num'>01</div>
-                                            </div>
-                                            <div className='gl_img'>
-                                                <img src="assets/img/gl2.png" alt="" />
+                        <div className="card_abt">
+                            <div className="card__In">
+                                <Fade direction="up" triggerOnce>
+                                    <div className='abt_Row abt_Row3'>
+                                        <div className='abt_gl-box'>
+                                            <div className='abt_gl-cnt'>
+                                                <div className='gl_ttl'>
+                                                    <h3>Lorem</h3>
+                                                </div>
+                                                <div className='gl_cn'>
+                                                    <div className='mn_ttl'>Ipsum</div>
+                                                    <div className='mn_Num'>01</div>
+                                                </div>
+                                                <div className='gl_img'>
+                                                    <img src="assets/img/gl3.png" alt="" />
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </Fade>
-
-                        </div>
-                    </div>
-                    <div className="card_abt">
-                        <div className="card__In">
-                            <Fade direction="up" triggerOnce>
-                                <div className='abt_Row abt_Row3'>
-                                    <div className='abt_gl-box'>
-                                        <div className='abt_gl-cnt'>
-                                            <div className='gl_ttl'>
-                                                <h3>Lorem</h3>
-                                            </div>
-                                            <div className='gl_cn'>
-                                                <div className='mn_ttl'>Ipsum</div>
-                                                <div className='mn_Num'>01</div>
-                                            </div>
-                                            <div className='gl_img'>
-                                                <img src="assets/img/gl3.png" alt="" />
-                                            </div>
+                                        <div className='abt_txt'>
+                                            <p>
+                                                "Lorem ipsum dolor sit amet,
+                                                consectetur adipiscing elit,
+                                                sed do eiusmod tempor incididunt
+                                                ut labore et dolore magna aliqua."
+                                            </p>
                                         </div>
                                     </div>
-                                    <div className='abt_txt'>
-                                        <p>
-                                            "Lorem ipsum dolor sit amet,
-                                            consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt
-                                            ut labore et dolore magna aliqua."
-                                        </p>
-                                    </div>
-                                </div>
-                            </Fade>
+                                </Fade>
 
+                            </div>
                         </div>
-                    </div>
 
-                </motion.div>
-            </div>
-        </section>
+                    </motion.div>
+                </div>
+            </section>
+        </>
     );
 };
+
+
+const MobScroll = () => {
+    return (
+        <div className="mob__Cards">
+            <div className="card_mob">
+                <Fade direction="up" triggerOnce>
+                    <div className='abt_Row abt_Row1'>
+                        <div className='abt_gl-box'>
+                            <div className='abt_gl-cnt'>
+                                <div className='gl_ttl'>
+                                    <h3>Lorem</h3>
+                                </div>
+                                <div className='gl_cn'>
+                                    <div className='mn_ttl'>Ipsum</div>
+                                    <div className='mn_Num'>01</div>
+                                </div>
+                                <div className='gl_img'>
+                                    <img src="assets/img/gl1.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='abt_txt'>
+                            <h2>
+                                About ProdServ<br></br><span>Technologies</span>
+                            </h2>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+            <div className="card_mob">
+                <Fade direction="up" triggerOnce>
+                    <div className='abt_Row abt_Row1'>
+                        <div className='abt_gl-box'>
+                            <div className='abt_gl-cnt'>
+                                <div className='gl_ttl'>
+                                    <h3>Lorem</h3>
+                                </div>
+                                <div className='gl_cn'>
+                                    <div className='mn_ttl'>Ipsum</div>
+                                    <div className='mn_Num'>01</div>
+                                </div>
+                                <div className='gl_img'>
+                                    <img src="assets/img/gl1.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='abt_txt'>
+                            <p>
+                                "Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua."
+                            </p>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+            <div className="card_mob">
+                <Fade direction="up" triggerOnce>
+                    <div className='abt_Row abt_Row1'>
+                        <div className='abt_gl-box'>
+                            <div className='abt_gl-cnt'>
+                                <div className='gl_ttl'>
+                                    <h3>Lorem</h3>
+                                </div>
+                                <div className='gl_cn'>
+                                    <div className='mn_ttl'>Ipsum</div>
+                                    <div className='mn_Num'>01</div>
+                                </div>
+                                <div className='gl_img'>
+                                    <img src="assets/img/gl1.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='abt_txt'>
+                            <p>
+                                "Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua."
+                            </p>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+        </div>
+    )
+}
 
 
 
