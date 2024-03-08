8d6e28603194ac11a01ce4670201b4e856d5dbd5
SarikAnwar01 <sarikanwar007@gmail.com>
2024-03-08 17:43:45 +0545

    fix(GlassSection background and fullstop from the title in feature)

--- src/components/Feature/Hero/Hero.jsx
+++ src/components/Feature/Hero/Hero.jsx
@@ -1,58 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import "./hero.css";
-import gsap from 'gsap';
+import gsap from "gsap";
 
 const Hero = () => {
-    useEffect(() => {
-        const tl = gsap.timeline();
+  useEffect(() => {
+    const tl = gsap.timeline();
 
-        tl.to(".ft_ttl-txt span", 1.8, {
-            y: 0,
-            ease: "power4.out",
-            delay: 0.5,
-            skewY: 0,
-            stagger: {
-                amount: 0.3
-            }
-        })
-
-    }, []);
-    return (
-        <div className='ft_Hero-wrap'>
-            <div className='ft_Hero-sec'>
-                <div className='ft_Hr-ttl'>
-                    <div className='ft_ttl-txt'>
-                        <span>
-                            One Stop
-                        </span>
-                    </div>
-                    <div className='ft_ttl-txt'>
-                        <span>
-                            Solution for
-                        </span>
-                    </div>
-                    <div className='ft_ttl-txt'>
-                        <span>
-                            Educational
-                        </span>
-                    </div>
-                    <div className='ft_ttl-txt'>
-                        <span>
-                            Management -
-                        </span>
-                    </div>
-                    <div className='ft_ttl-txt'>
-                        <span>
-                            Feature Odyssey.
-                        </span>
-                    </div>
-                </div>
-                <div className='ft_Hr-img'>
-                    <img src="assets/img/hr-img.png" alt="" />
-                </div>
-            </div>
+    tl.to(".ft_ttl-txt span", 1.8, {
+      y: 0,
+      ease: "power4.out",
+      delay: 0.5,
+      skewY: 0,
+      stagger: {
+        amount: 0.3,
+      },
+    });
+  }, []);
+  return (
+    <div className="ft_Hero-wrap">
+      <div className="ft_Hero-sec">
+        <div className="ft_Hr-ttl">
+          <div className="ft_ttl-txt">
+            <span>One Stop</span>
+          </div>
+          <div className="ft_ttl-txt">
+            <span>Solution for</span>
+          </div>
+          <div className="ft_ttl-txt">
+            <span>Educational</span>
+          </div>
+          <div className="ft_ttl-txt">
+            <span>Management -</span>
+          </div>
+          <div className="ft_ttl-txt">
+            <span>Feature Odyssey</span>
+          </div>
         </div>
-    )
-}
+        <div className="ft_Hr-img">
+          <img src="assets/img/hr-img.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
