070b23e271bdf7d92343212e1d1d9fd72308c4bc
SarikAnwar01 <sarikanwar007@gmail.com>
2024-03-08 23:17:30 +0545

    test

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -6,24 +6,23 @@ import { Fade } from "react-awesome-reveal";
 
 import { Link } from "react-router-dom";
 
-import gsap from 'gsap';
+import gsap from "gsap";
 
 const Hero = () => {
-
   // title text reveal
   useEffect(() => {
     const tl = gsap.timeline();
 
-    tl.to(".hr_ttl-txt span", 1.8, {
+    tl.to(".hr_ttl-txt span", {
+      duration: 1.8,
       y: 0,
       ease: "power4.out",
       delay: 0.5,
       skewY: 0,
       stagger: {
-        amount: 0.3
-      }
-    })
-
+        amount: 0.3,
+      },
+    });
   }, []);
 
   return (
@@ -31,7 +30,6 @@ const Hero = () => {
       <div className="Hero-container">
         <div className="hr__Sec-wrap">
           <div className="hero__Ttl">
-
             <div className="hr_ttl-txt">
               <span>CRAFTED AND</span>
             </div>
@@ -53,7 +51,6 @@ const Hero = () => {
             </div>
           </div>
         </div>
-
       </div>
       <SubHero />
     </>
