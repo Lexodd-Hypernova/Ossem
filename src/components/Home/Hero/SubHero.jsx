9caf97a18c0430a91bc87b73b27545164def5ba9
SarikAnwar01 <sarikanwar007@gmail.com>
2024-03-09 20:45:57 +0545

    fix(text reveal disabled)

--- src/components/Home/Hero/SubHero.jsx
+++ src/components/Home/Hero/SubHero.jsx
@@ -6,16 +6,21 @@ import { Fade } from "react-awesome-reveal";
 
 import TextReveal from "../../TextReveal";
 
-const textReveal = "We are evolved to make it easy for you to manage your organization by providing the right set of tools at the right place. Onboard and experience the transformation that you wished for. With us you can Learn, Grow and Succeed.";
-
+const textReveal =
+  "We are evolved to make it easy for you to manage your organization by providing the right set of tools at the right place. Onboard and experience the transformation that you wished for. With us you can Learn, Grow and Succeed.";
 
 const SubHero = () => {
   return (
     <>
       <div className="Sub-Hero">
         <div className="sb_hr_Row1">
-
-          <TextReveal text={textReveal}></TextReveal>
+          {/* <TextReveal text={textReveal}></TextReveal> */}
+          <p
+          // ref={splitTypesRef}
+          // className="reveal-type"
+          >
+            {textReveal}
+          </p>
 
           {/* <p>
             <Fade direction="up" triggerOnce>
@@ -36,20 +41,20 @@ const SubHero = () => {
           <h2>
             With <span className="ttl_stl1">Ossem</span> You
             <br></br>
-            <span className="ttl_stl2">Build to </span><span className="ttl_stl3">Scale</span>
+            <span className="ttl_stl2">Build to </span>
+            <span className="ttl_stl3">Scale</span>
           </h2>
           <p>
             Our mission is to make management easy and effective for everyone,
-            providing specially curated learning resources and a transformative platform.
+            providing specially curated learning resources and a transformative
+            platform.
           </p>
         </div>
         <div className="bld__hr-img">
           <img src={MacIng} alt="mac" />
         </div>
         <div className="bld_ext-p">
-          <p>
-            Ossem: Bulit for Effective Management
-          </p>
+          <p>Ossem: Bulit for Effective Management</p>
         </div>
       </div>
     </>
