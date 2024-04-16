f568be37089e288e79228b3c7ca2e2359a514f99
SarikAnwar01 <sarikanwar007@gmail.com>
2024-04-16 11:17:28 +0545

    update (removal of founder section  from about page )

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,41 +1,40 @@
-import React, { useEffect } from 'react';
-import Hero from '../components/About/Hero';
-import Technology from '../components/About/Technology';
+import React, { useEffect } from "react";
+import Hero from "../components/About/Hero";
+import Technology from "../components/About/Technology";
 import Cliente from "../components/Home/Cliente/Cliente";
-import Founder from '../components/About/Founder';
-import AboutVideoFile from '../components/About/AboutVideoFile';
+// import Founder from "../components/About/Founder";
+import AboutVideoFile from "../components/About/AboutVideoFile";
 
-import Lenis from '@studio-freight/lenis';
+import Lenis from "@studio-freight/lenis";
 
 import ScrollToTop from "../components/ScrollToTop";
 
 const About = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
 
-    useEffect(() => {
-        const lenis = new Lenis()
+    lenis.on("scroll", (e) => {
+      // console.log(e)
+    });
 
-        lenis.on('scroll', (e) => {
-            // console.log(e)
-        })
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-        function raf(time) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
-        }
+    requestAnimationFrame(raf);
+  }, []);
 
-        requestAnimationFrame(raf)
-    }, [])
+  return (
+    <>
+      <Hero></Hero>
+      <Technology></Technology>
+      <AboutVideoFile></AboutVideoFile>
+      {/* <Founder></Founder> */}
+      <Cliente></Cliente>
+      <ScrollToTop></ScrollToTop>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Hero></Hero>
-            <Technology></Technology>
-            <AboutVideoFile></AboutVideoFile>
-            <Founder></Founder>
-            <Cliente></Cliente>
-            <ScrollToTop></ScrollToTop>
-        </>
-    )
-}
-
-export default About
+export default About;
