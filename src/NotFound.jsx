249f6417ab762ebfa0e9e004d90e84d5a1ebd397
SarikAnwar01 <sarikanwar007@gmail.com>
2024-05-05 00:16:25 +0545

    404

---
+++ src/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <p>Website Down Due To Technical Difficulties</p>
+      <p>Contact Dev Team for Support Thank You !</p>
+    </div>
+  );
+};
+
+export default NotFound;
