9f25c08207a9d4c9e726ec6181e113687df9da95
SarikAnwar01 <sarikanwar007@gmail.com>
2024-05-13 21:16:01 +0545

    Fix(ossem reinstate)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import NotFound from "./NotFound.jsx";
+// import NotFound from "./NotFound.jsx";
 import "./Fonts.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <NotFound />
+    <App />
+    {/* <NotFound /> */}
   </React.StrictMode>
 );
